89892e48965756e8f2afa3f6bc60d38f9e932a19
Yeon <68737910+YeonIsFree@users.noreply.github.com>
2024-01-08 20:36:21 +0900

    SESAC 10회차 과제

---
+++ SESAC-TableView/TravelTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTableViewCell.swift
+//  SESAC-TableView
+//
+//  Created by Seryun Chun on 2024/01/08.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    @IBOutlet var mainImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+}
---
+++ SESAC-TableView/TravelTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TravelTableViewController.swift
+//  SESAC-TableView
+//
+//  Created by Seryun Chun on 2024/01/08.
+//
+
+import UIKit
+import Kingfisher
+
+class TravelTableViewController: UITableViewController {
+    
+    let magazineList: [Magazine] = MagazineInfo().magazine
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return magazineList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TravelTableViewCell", for: indexPath) as! TravelTableViewCell
+        
+        cell.mainImageView.layer.cornerRadius = 20
+        
+        let magazine: Magazine = magazineList[indexPath.row]
+        
+        // imageView
+        let url = URL(string: magazine.photo_image)!
+        cell.mainImageView.kf.setImage(with: url)
+        
+        // title
+        cell.titleLabel.text = magazine.title
+        
+        // subtitle
+        cell.subtitleLabel.text = magazine.subtitle
+        
+        // dateLabel - dateformatter
+        let dateString = magazine.date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyMMdd"
+        let convertedDate = dateFormatter.date(from: dateString)
+        
+        let myDateFormatter = DateFormatter()
+        myDateFormatter.dateFormat = "yy년 MM월 dd일"
+        cell.dateLabel.text = myDateFormatter.string(from: convertedDate!)
+        
+        return cell
+    }
+
+}
